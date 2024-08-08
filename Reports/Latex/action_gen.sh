3843a08fd03c9d3677fdb017d74a72ebabfc2b9d
gaurovgiri <gaurovgiri@gmail.com>
2024-08-08 09:29:20 +0545

    add absolute path to fix workflow

---
+++ Reports/Latex/action_gen.sh
@@ -0,0 +1,4 @@
+echo 'R' | pdflatex -f Reports/Latex/KECReport.tex --interaction=nonstopmode --synctex=1
+bibtex KECReport
+echo 'R' | pdflatex -f Reports/Latex/KECReport.tex --interaction=nonstopmode --synctex=1
+echo 'R' | pdflatex -f Reports/Latex/KECReport.tex --interaction=nonstopmode --synctex=1
